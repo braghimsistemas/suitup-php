a04456756245617329068199b69f7b9604366a05
Marco A. Braghim <marco.a.braghim@gmail.com>
2016-09-20 18:05:28 -0300

    Adicionando testes unitarios ao aplicativo

--- test/setup.php
+++ test/setup.php
@@ -6,13 +6,15 @@ if (is_dir('../../vendor')) {
 
 } else if (is_dir('../../../vendor')) {
 	chdir('../../../vendor');
+} else if (is_dir('../vendor')) {
+	chdir('../vendor');
 }
 
 if (file_exists('autoload.php')) {
 	include 'autoload.php';
 } else {
 	echo "\n\n";
-	print_r(scandir('/home/travis/build'));
+	print_r(scandir('../'));
 	echo "\n\n";
 }
 
