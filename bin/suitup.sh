718c3c15f03d3449b85dfa722afe8488606bafcc
Pernalonga <marco.a.braghim@gmail.com>
2018-09-19 00:03:07 -0300

    Ready to create modules, controllers and forms

--- bin/suitup.sh
+++ bin/suitup.sh
@@ -2,15 +2,18 @@
 ###################################################################
 #Script Name	: suitup
 #Description	: SuitUp Manager
-#Args           : create module <name>,
-#                 create controller <module> <name>
-#                 create action <module> <controller> <name>
-#Author       	: Braghim Sistemas
-#Email         	: braghim.sistemas@gmail.com
+#Args         : create module <name>,
+#               create controller <name> <module>,
+#               create form <name> <module>,
+#               create project <folder>,
+#               dbtable <dbname> <module> <pk1> <pk2> <pk3>...
+#               
+#Author       : Braghim Sistemas
+#Email        : braghim.sistemas@gmail.com
 ###################################################################
 
 version="1.0.0"
-echo "SuitUp Manager - Version: $version"
+printf "SuitUp Manager - Version: $version\n\n"
 
 # Show to the user the help document options
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
@@ -35,11 +38,21 @@ function capitalize() {
   printf "%s" "${word^}"
 }
 
+# Remove empty spaces from a string
+# link: https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
+function trim() {
+  local var="$*"
+  # remove leading whitespace characters
+  var="${var#"${var%%[![:space:]]*}"}"
+  # remove trailing whitespace characters
+  var="${var%"${var##*[![:space:]]}"}"   
+  echo -n "$var"
+}
+
 function createNewModule() {
 
   local path=$2              # Path to the modules
   local name="Module""$1"    # Name of the new module
-  local src=$3               # Realpath to this script (where we get the models...)
 
   # Create folders
   mkdir -p "$path""/""$name""/Controllers/"
@@ -98,16 +111,19 @@ cat <<EOF > "$path""/""$name""/views/layout.phtml"
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>New Module: $name</h1>
-    <h4>Created by SuitUp Manager Version: $version</h4>
+    <div class="container">
+      <h1>$name</h1>
+      <h4>Created by SuitUp Manager Version: $version</h4>
 
-    <?php echo \$content; ?>
+      <?php echo \$content; ?>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.0/holder.js"></script>
   </body>
 </html>
 
@@ -117,25 +133,153 @@ EOF
 cat <<EOF > "$path""/""$name""/views/index/index.phtml"
 <!-- The content created automatically -->
 <div class="row">
-  <div class="col-6 mx-auto">
-
+  <div class="col-6">
     <div class="card">
-      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+      <img class="card-img-top" src="holder.js/100px180/" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
-
+  </div>
+  <div class="col-6">
+    <div class="card">
+      <img class="card-img-top" src="holder.js/100px180/" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   </div>
 </div>
 
 EOF
 
-  printf "# SuitUp Manager \n\nThat's pretty awesome\nThis module was created automatically" > "$path""/""$name""/readme.md"
-
   echo "Done. Enjoy your new module =)"
+}
+
+# This function will generate files and folders to a new controller (with view)
+function createController() {
+
+  local name="$1""Controller"       # Name of the new controller
+  local viewName="${1,,}"           # The view name (lowercase)
+  local module=$2                   # Name of the module (Namespace)
+  local path=$3                     # Path to the modules
+
+  # Create folders if does not exist
+  mkdir -p "$path""/""$module""/Controllers/"
+  mkdir -p "$path""/""$module""/views/""$viewName""/"
+
+  # Check if controller already exists
+  if [ -f "$path""/""$module""/Controllers/$name.php" ]; then
+    echo "It's embarrassing but you already have a '""$path""/""$module""/Controllers/$name.php""' file õ.O"
+    echo "Aborting..."
+    exit 1
+  fi
+
+# The controller
+cat <<EOF > "$path""/""$module""/Controllers/$name.php"
+<?php
+namespace $module\Controllers;
+
+use SuitUp\Mvc\MvcAbstractController;
+
+class $name extends MvcAbstractController
+{
+  public function indexAction() {
+    // Here begins the legends...
+  }
+}
+
+EOF
+
+# index/index.phtml
+cat <<EOF > "$path""/""$module""/views/""$viewName""/index.phtml"
+<!-- The content created automatically -->
+<div class="row">
+  <div class="col-6">
+    <div class="card">
+      <img class="card-img-top" src="holder.js/100px180/" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-6">
+    <div class="card">
+      <img class="card-img-top" src="holder.js/100px180/" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+EOF
+
+  echo "Done. We also created the view file for you"
+
+}
+
+# Create a form file for a module
+function createForm() {
+
+  local name=$1                     # Name of the new form
+  local module=$2                   # Name of the module (Namespace)
+  local path=$3                     # Path to the modules
+
+  # Split into array
+  readarray -d / -t parts <<< "$name"
+
+  folder="$path""/""$module""/Form/"
+  namespace="$module""\\Form"
+  if [ ${#parts[@]} -eq 2 ]; then
+    
+    # Redefine variables
+    name=$(trim "${parts[1]}")
+    folder="$path""/""$module""/Form/""${parts[0]}"
+    namespace="$module""\\Form\\""${parts[0]}"
+  fi
+
+  # Validate file exists
+  if [ -f "$folder""/""$name"".php" ]; then
+    echo "The file '""$folder""/""$name"".php""' already exists, nothing changed"
+    exit 1
+  fi
+
+  # Create if folder doesnt exists
+  if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+  fi
+
+# Create the file itself
+cat <<EOF > "$folder""/""$name"".php"
+<?php 
+namespace $namespace;
+
+use SuitUp\FormValidator\AbstractFormValidator;
+
+class $name extends AbstractFormValidator
+{
+
+  /**
+   * @var array 
+   */
+  protected $data = array(
+    // Your validations here
+  );
+}
+
+EOF
+
+  echo "Done. Yeah, it was legen... { wait for it } ...dary!"
+
 }
 
 ###################################################################
@@ -219,7 +363,7 @@ then
     then
       
       # The function that will create the module
-      createNewModule $name $modulesPath $(dirname $SOURCE)
+      createNewModule $name $modulesPath
       exit 0
 
     else
@@ -235,7 +379,124 @@ then
     # CREATE NEW CONTROLLER #
     #########################
 
-    echo "oi"
+    # Get the third param or request it from user
+    name=$3
+    if [ "$name" = "" ]
+    then
+      echo "Which is the name of the new controller?"
+      read name
+    fi
+
+    # Prevent wrong type
+    name=$(capitalize "$name")
+
+    # Get the fourth param or request it from user
+    module=$4
+
+    while true
+    do
+
+      if [ "$module" = "" ]
+      then
+        echo "Which is the name of the module where you wanna do it?"
+        read module
+      fi
+
+      # Prevent wrong type
+      module="Module"$(capitalize "$module")
+
+      if [ ! -d "$modulesPath""/""$module" ]
+      then
+        echo "It's embarrassing, but seems '$module' folder does not exists, let's try again..."
+        module=""
+      else
+        break
+      fi
+
+    done
+
+    # Ask if is this really what he wanna do
+    echo "Create a new controller named '""$name""Controller' in the module '""$module""' (y/N)"
+    read allow
+
+    # User is pretty sure to append this new module...
+    if [ "$allow" = "y" ] || [ "$allow" = "Y" ] || [ "$allow" = "yes" ] || [ "$allow" = "Yes" ]
+    then
+      
+      # The function that will create the module
+      createController $name $module $modulesPath
+      exit 0
+
+    else
+      echo "Answer: '$allow'. Ok, nothing was changed, bye"
+      exit 0
+    fi
+
+  elif [ "$2" = "form" ]
+  then
+    
+    ###################
+    # CREATE NEW FORM #
+    ###################
+
+    # Get the third param or request it from user
+    name=$3
+    if [ "$name" = "" ]
+    then
+      echo "Which is the name of the new form? We recommend you something like 'controller/action'"
+      read name
+    fi
+    name=$(capitalize "$name")
+
+    # It's recommended to create forms with a sub folder like "Auth/Login"
+    readarray -d / -t partsFormName <<< "$name"
+    if [ ${#partsFormName[@]} -eq 2 ]; then
+      name=$(capitalize ${partsFormName[0]})"/"$(capitalize ${partsFormName[1]})
+    fi
+
+    # Get the fourth param or request it from user
+    module=$4
+
+    while true
+    do
+
+      if [ "$module" = "" ]
+      then
+        echo "Which is the name of the module where you wanna do it?"
+        read module
+      fi
+
+      # Prevent wrong type
+      module="Module"$(capitalize "$module")
+
+      if [ ! -d "$modulesPath""/""$module" ]
+      then
+        echo "It's embarrassing, but seems '$module' folder does not exists, let's try again..."
+        module=""
+      else
+        break
+      fi
+
+    done
+
+    # Ask if is this really what he wanna do
+    echo "Create a new form named '""$name""' in the module '""$module""' (y/N)"
+    read allow
+
+    # User is pretty sure to append this new module...
+    if [ "$allow" = "y" ] || [ "$allow" = "Y" ] || [ "$allow" = "yes" ] || [ "$allow" = "Yes" ]
+    then
+      
+      # The function that will create the module
+      createForm $name $module $modulesPath
+      exit 0
+
+    else
+      echo "Answer: '$allow'. Ok, nothing was changed, bye"
+      exit 0
+    fi
+
+  fi
 
   elif [ "$2" = "" ]
   then
