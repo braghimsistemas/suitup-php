b75621c7a8712db369c0fb35dafe65145310e6f5
Marco A. Braghim <marco.a.braghim@gmail.com>
2018-09-22 09:14:08 -0300

    New version tag

--- src/SuitUpStart.php
+++ src/SuitUpStart.php
@@ -60,7 +60,7 @@ class SuitUpStart
   /**
    * Versão atual do sistema
    */
-  const VERSION = '1.2.2';
+  const VERSION = '1.2.4';
 
   /**
    * Caminho, no projeto do usuario, onde se encontram as pastas dos módulos.
